c8e77c541a0aa786c333dc533e435a19cdd42179
A. Conrad Nied <anied@cs.washington.edu>
2016-02-11 10:17:18 -0800

    Generalize the coding upload

--- scripts/python/uploadCodes.py
+++ scripts/python/uploadCodes.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 
-import json, os, re, csv, sys, socket
-import argparse, unicodedata
+import json, re, argparse
 from pprint import pprint
-from datetime import datetime, timedelta
 from server_messenger import ServerMessenger
 import mysql.connector
 import gspread # installed via pip install gspread
@@ -16,14 +14,16 @@ googleAPI = None
 # Queries
 query_get_coder = ("SELECT * FROM Coder "
                    "WHERE ShortName = %(name)s ; ")
-query_push_code = ("INSERT IGNORE INTO Code"
-                   "(Coder, Rumor, Tweet, Period, Uncodable, Unrelated, Affirm, Deny, Neutral, Implicit, Ambiguity, Uncertainty, Difficult, Text)"
-                  "VALUES (%(Coder)s, %(Rumor)s, %(Tweet)s, %(Period)s, %(Uncodable)s, %(Unrelated)s, %(Affirm)s, %(Deny)s, %(Neutral)s, %(Implicit)s, %(Ambiguity)s, %(Uncertainty)s, %(Difficult)s, %(text)s); ")
-
-query_add_tweet = ("INSERT INTO Tweet "
-             "(ID, Text, `Distinct`, Type, Username, Timestamp, Origin) "
-             "VALUES (%(ID)s, %(Text)s, %(Distinct)s, %(Type)s, %(Username)s, %(Timestamp)s, %(Origin)s) "
-            "ON DUPLICATE KEY UPDATE `Distinct`=%(Distinct)s, `Text`=%(Text)s ")
+query_push_code = ("INSERT IGNORE INTO Code "
+                   "(Rumor, Period, Tweet, Coder, "
+                   " Uncodable, Unrelated, Affirm, Deny, Neutral, "
+                   " Implicit, Ambiguity, Uncertainty, Difficult, Text) "
+                   "VALUES (%(Rumor)s, %(Period)s, %(Tweet)s, %(Coder)s, "
+                   " %(Uncodable)s, %(Unrelated)s, %(Affirm)s, %(Deny)s, %(Neutral)s, "
+                   " %(Implicit)s, %(Ambiguity)s, %(Uncertainty)s, %(Difficult)s, %(text)s); ")
+query_add_tweet = ("INSERT IGNORE INTO Tweet "
+                   "(ID, Text, `Distinct`, Type, Username, Timestamp, Origin) "
+                   "VALUES (%(ID)s, %(Text)s, %(Distinct)s, %(Type)s, %(Username)s, %(Timestamp)s, %(Origin)s) ")
 
 def main():
     parser = argparse.ArgumentParser(description='Parse a raw collection into the important constituents and/or calculate statistics on the collection',
@@ -38,11 +38,21 @@ def main():
     connectToServer()
     cursor = serverStorage.cursor()
     
-    search_title = 'Umpqua Crisis Actors Coding Sheet'
-    rumor_id = 5
+    # Set parameters
+    event_name = "Paris" # Umpqua Paris
+    rumor_name = "Bot Prediction" # Crisis Actors Bot Prediction
+    period_name = "Training" # Coding Training
     
+    # 1 Paris Bot Prediction
+    # 5 Umpqua Crisis Actors
+    # 6 Paris Crisis Actors
     
+    rumor_id = 1
+    period_id = -1
     
+    search_title = ' ' + event_name + ' ' + rumor_name + ' ' + period_name
+    
+    # Open spreadsheets
 #    spreadsheet = googleAPI.open('Conrad Umpqua Crisis Actors Coding Sheet')
     sheets = googleAPI.openall();
     for sheet in sheets:
@@ -61,16 +71,16 @@ def main():
             codes = worksheet.get_all_records()
             
             for code in codes:
-                code['Coder'] = coder_id
-                code['Rumor'] = rumor_id
-                code['Tweet'] = code['tweet_id']
-                code['Period'] = 'Primary'
+                code['Coder']     = coder_id
+                code['Rumor']     = rumor_id
+                code['Tweet']     = code['tweet_id']
+                code['Period']    = period_id
                 code['Uncodable'] = bool(code['Uncodable'])
                 code['Unrelated'] = bool(code['Unrelated'])
-                code['Affirm'] = bool(code['Affirm'])
-                code['Deny'] = bool(code['Deny'])
-                code['Neutral'] = bool(code['Neutral'])
-                code['Implicit'] = bool(code['Implicit'])
+                code['Affirm']    = bool(code['Affirm'])
+                code['Deny']      = bool(code['Deny'])
+                code['Neutral']   = bool(code['Neutral'])
+                code['Implicit']  = bool(code['Implicit'])
                 code['Ambiguity'] = bool(code['Ambiguity'])
                 code['Uncertainty'] = bool(code['Uncertainty'])
                 code['Difficult'] = bool(code['Difficult'])
@@ -86,13 +96,7 @@ def connectToServer():
     if(options.verbose): print("    Connecting to Captures Database")
     
     with open(options.config) as config_file:
-        global serverCapture
         config = json.load(config_file)
-        
-        serverCapture = ServerMessenger(
-            base_url = config["server"]["base_url"],
-            token = config["server"]["token"]
-        )
         
         # MySQL Storage
         global serverStorage
@@ -105,7 +109,6 @@ def connectToServer():
             
         global googleAPI
         scope = ['https://spreadsheets.google.com/feeds']
-#        scope = ['https://docs.google.com/spreadsheets']
         credentials = SignedJwtAssertionCredentials(config["google_api"]['client_email'], config["google_api"]['private_key'].encode(), scope)
         googleAPI = gspread.authorize(credentials)
 
