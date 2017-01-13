5cc76f081c92b93973f497ecac1a415c1387e547
A. Conrad Nied <conrad.logos@gmail.com>
2017-01-12 23:15:27 -0800

    Open up script to more users

--- src/php/follows/getUserIDs.php
+++ src/php/follows/getUserIDs.php
@@ -2,10 +2,10 @@
     include '../connect.php';
 
     // Execute Query
-//    $query = "SELECT UserID ".
-//             "FROM UserDataQueue; ";
     $query = "SELECT UserID ".
-             "FROM UserInSubset WHERE SubsetID=2637; ";
+             "FROM UserDataQueue; ";
+//    $query = "SELECT UserID ".
+//             "FROM UserInSubset WHERE SubsetID=2637; ";
 
     include '../printResults.php';
 ?>
